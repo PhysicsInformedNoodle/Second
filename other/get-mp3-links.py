da4ef6061e39f97a0f017864c7c01d18dec0064d
PhysicsInformedNoodleNetwork <pe.dro.delf@gmail.com>
2023-11-13 01:27:14 +0000

    Create get-mp3-links.py .

---
+++ other/get-mp3-links.py
@@ -0,0 +1,22 @@
+
+import requests
+from bs4 import BeautifulSoup
+
+def get_mp3_links(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    mp3_links = [a['href'] for a in soup.find_all('a', href=True) if a['href'].endswith('.mp3')]
+
+    return mp3_links
+
+# use it like this
+
+
+url = 'https://www.google.com/' # replace with the URL you want
+
+mp3_links = get_mp3_links(url)
+
+# print each mp3 link
+for link in mp3_links:
+    print(link)
